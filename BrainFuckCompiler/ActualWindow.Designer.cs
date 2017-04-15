52714436850a9f4b20060978b3bc8524ef1b1115
Rolf Plagmeijer <sausage948@gmail.com>
2017-04-16 01:22:25 +0200

    Project Added.

---
+++ BrainFuckCompiler/ActualWindow.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace BrainFuckCompiler
+{
+	partial class ActualWindow
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.startButton = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// startButton
+			// 
+			this.startButton.Location = new System.Drawing.Point(12, 346);
+			this.startButton.Name = "startButton";
+			this.startButton.Size = new System.Drawing.Size(120, 25);
+			this.startButton.TabIndex = 0;
+			this.startButton.Text = "START";
+			this.startButton.UseVisualStyleBackColor = true;
+			this.startButton.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(137, 346);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(120, 25);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "button2";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// ActualWindow
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1477, 680);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.startButton);
+			this.Name = "ActualWindow";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.ActualWindow_Load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button startButton;
+		private System.Windows.Forms.Button button2;
+	}
+}
+
